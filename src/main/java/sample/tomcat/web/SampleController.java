ad2e0ad3cbf7eacb96d05bc9290a4e6098c03543
userrepo <knelakurti@miraclesoft.com>
2016-09-16 14:15:47 -0500

    Update SampleController.java

--- src/main/java/sample/tomcat/web/SampleController.java
+++ src/main/java/sample/tomcat/web/SampleController.java
@@ -20,7 +20,7 @@ import sample.tomcat.service.HelloWorldService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
